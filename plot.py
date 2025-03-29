b9c078f4f040a1de123bb6e53b84c10b20a3851b
Jake Taylor <43712099+jakee417@users.noreply.github.com>
2025-03-29 11:41:55 -0700

    add subplots

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import plotly.express as px
 import pandas as pd
+import plotly.subplots as sp
 
 A = 1
 L = 1
@@ -13,20 +14,34 @@ q = A * np.sin(omega * t + phi)
 i = omega * A * np.cos(omega * t + phi)
 v_L = -np.pow(omega, 2) * L * A * np.sin(omega * t + phi)
 v_C = q / C
-H = 0.5 * C * np.power(v_C, 2) + 0.5 * L * np.power(i, 2)
-df = pd.DataFrame(
-    {"t": t, "q(t)": q, "i(t)": i, "v_L(t)": v_L, "v_C(t)": v_C, "H(t)": H}
+fig1 = px.line(
+    pd.DataFrame({"t": t, "q(t)": q, "i(t)": i, "v_L(t)": v_L, "v_C(t)": v_C}),
+    x="t",
+    y=["q(t)", "i(t)", "v_L(t)", "v_C(t)"],
 )
-fig = px.line(df, x="t", y=["q(t)", "i(t)", "v_L(t)", "v_C(t)", "H(t)"])
+E_C = 0.5 * C * np.power(v_C, 2)
+E_L = 0.5 * L * np.power(i, 2)
+H = E_C + E_L
+fig2 = px.line(
+    pd.DataFrame({"t": t, "E_C(t)": E_C, "E_L(t)": E_L, "H(t)": H}),
+    x="t",
+    y=["E_C(t)", "E_L(t)", "H(t)"],
+)
+
+# Create a 1x2 subplot
+fig = sp.make_subplots(rows=2, cols=1)
+
+# Get the Express fig broken down as traces and add the traces to the proper plot within in the subplot
+for trace in range(len(fig1["data"])):
+    fig.append_trace(fig1["data"][trace], row=1, col=1)
+
+for trace in range(len(fig2["data"])):
+    fig.append_trace(fig2["data"][trace], row=2, col=1)
+
 snapshots = [("T / 4", T / 4), ("T / 2", T / 2), ("T * 3 / 4", T * 3 / 4)]
-for i, x in snapshots:
+for l, x in snapshots:
     fig.add_vline(
-        x=x, line_width=3, line_dash="dash", line_color="red", annotation_text=f"t={i}"
+        x=x, line_width=3, line_dash="dash", line_color="red", annotation_text=f"t={l}"
     )
 fig.add_hline(y=0)
-fig.update_layout(
-    legend=dict(
-        orientation="h", entrywidth=70, yanchor="bottom", y=1.02, xanchor="right", x=1
-    )
-)
 fig.show()
