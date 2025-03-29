a7eae2651c23202fea1fd2ec8b8091a7d9d56051
Jake Taylor <43712099+jakee417@users.noreply.github.com>
2025-03-28 17:59:54 -0700

    Update plot.py

--- plot.py
+++ plot.py
@@ -2,8 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 import pandas as pd
-import plotly.graph_objects as go
-from IPython.display import display, Markdown
 A = 1
 L = 1
 C = 1
@@ -14,7 +12,8 @@ t = np.linspace(0, T)
 q = A * np.sin(omega * t + phi)
 i = omega * A * np.cos(omega * t + phi)
 v = -np.pow(omega, 2) * L * A * np.sin(omega * t + phi)
-fig = px.line(pd.DataFrame({"t": t, "q(t)": q, "i(t)": i, "v(t)": v}), x="t", y=["q(t)", "i(t)", "v(t)"])
+H = 0.5 * C * np.power(v, 2) + 0.5 * L * np.power(i, 2)
+fig = px.line(pd.DataFrame({"t": t, "q(t)": q, "i(t)": i, "v(t)": v, "H(t)": H}), x="t", y=["q(t)", "i(t)", "v(t)", "H(t)"])
 for i, x in [("T / 4", T / 4), ("T / 2", T / 2), ("T * 3 / 4", T * 3 / 4)]:
     fig.add_vline(x=x, line_width=3, line_dash="dash", line_color="red", annotation_text=f"t={i}")
 fig.add_hline(y=0)
